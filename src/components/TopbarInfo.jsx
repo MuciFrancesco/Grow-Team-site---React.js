dbcbc25233606f1c4121929a936f381fdc7a95ce
francesco <itachi1200@gmail.com>
2022-06-25 11:13:11 +0200

    add-last-scss-query

--- src/components/TopbarInfo.jsx
+++ src/components/TopbarInfo.jsx
@@ -5,9 +5,9 @@ function TopbarInfo({returnUpPage}) {
   return (
     <div ref={returnUpPage} className='topbar-info'>
       <img src={Grow} alt="" />
-      <a className='mail'  href="mailto:francesco.muci@hotmail.it">francesco.muci@hotmail.it</a>
+      {window.innerWidth>380?<a className='mail'  href="mailto:francesco.muci@hotmail.it">francesco.muci@hotmail.it</a>:<a className='mail'  href="mailto:francesco.muci@hotmail.it" style={{margin:"1rem", fontSize:"12px"}}>francesco.muci@hotmail.it</a>}
       <p className='divide'>|</p>
-      <p className='cell'>Chiamaci per maggiori informazioni:<span><a className='cell-a' href='tel:3282397191'>328 2397191</a></span></p>
+      {window.innerWidth>380?<p className='cell'>Chiamaci per maggiori informazioni:<span><a className='cell-a' href='tel:3282397191'>328 2397191</a></span></p>:null}
     </div>
   )
 }
