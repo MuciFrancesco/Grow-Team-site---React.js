6588921d6bab7eb74448f385719542691f2ee7b9
francesco <itachi1200@gmail.com>
2022-06-26 19:30:35 +0200

    modify-links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useCallback } from 'react'
 import logo from "../assets/logo.png"
 import facebook from "../assets/facebook.svg"
 import twitter from "../assets/twitter.svg"
@@ -11,6 +11,9 @@ import { Link } from 'react-router-dom'
 
 
 function Footer({handleScroll}) {
+  const OpenToTopPage=useCallback(()=>{
+    window.scroll(0,0)
+},[])
   return (
     <>
     <div className='footer'>
@@ -37,17 +40,17 @@ function Footer({handleScroll}) {
       <div className='footer-links'>
         <ul>
           <h3>Service</h3>
-          <Link to="/about-us"><li><a href="">Storia dell'azienda</a></li></Link>
-          <Link to="/about-us"><li><a href="">About Us</a></li></Link>
-          <Link to="/services"><li><a href="">Servizi</a></li></Link>
-          <Link to="/"><li><a href="#">Politica Privacy</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/about-us"><li><a href="">Storia dell'azienda</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/about-us"><li><a href="">About Us</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/services"><li><a href="">Servizi</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/"><li><a href="#">Politica Privacy</a></li></Link>
         </ul>
         <ul>
           <h3>Info</h3>
-          <Link to="/"><li><a href="">La nostra missione</a></li></Link>
-          <Link to="/services"><li><a href="">I nostri obiettivi</a></li></Link>
-          <Link to="/about-us"><li><a href="">Chi siamo</a></li></Link>  
-          <Link to="/team"><li><a href="">Il nostro Team</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/"><li><a href="">La nostra missione</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/services"><li><a href="">I nostri obiettivi</a></li></Link>
+          <Link onClick={OpenToTopPage} to="/about-us"><li><a href="">Chi siamo</a></li></Link>  
+          <Link onClick={OpenToTopPage} to="/team"><li><a href="">Il nostro Team</a></li></Link>
         </ul>
       </div>
       <div className='footer-subsribe'>
